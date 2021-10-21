06e20e4b82d98d06cda2ea37645497db7206bf43
dorF <nemoz.burkhalter@gmail.com>
2021-10-21 15:35:49 +0200

    Started on chapters

---
+++ Game Book Adaptation/Projet Nem/chapter_node.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+#include <vector>
+
+class chapter_node
+{
+	//The text that is going to be shown to the player
+	std::string text_;
+
+	std::vector<int> destinations_;
+public:
+	chapter_node(std::string text, std::vector<int> destinations);
+}; 
